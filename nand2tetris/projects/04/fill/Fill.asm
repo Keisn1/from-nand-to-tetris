728d26e7c949508bf7217bb7403827b8f5a31047
Kay Freyer <kay.freyer@icloud.com>
2024-02-21 10:26:05 +0100

    End week 4

--- nand2tetris/projects/04/fill/Fill.asm
+++ nand2tetris/projects/04/fill/Fill.asm
@@ -12,3 +12,88 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// Init
+    @color
+    M=0
+
+    (READ_INPUT)
+    @KBD
+    D=M
+
+    @WHITE
+    D;JEQ
+
+    (BLACK)
+        @color
+        D=M
+
+        // Screen already black? Go back to READ_INPUT
+        @READ_INPUT
+        D+1;JEQ
+
+        @color
+        M=-1
+
+        @FILL_SCREEN
+        0;JMP
+
+    (WHITE)
+        @color
+        D=M
+
+        // Screen already white? Go back to READ_INPUT
+        @READ_INPUT
+        D;JEQ
+
+        @color
+        M=0
+
+        @FILL_SCREEN
+        0;JMP
+
+    (FILL_SCREEN)
+        // Initialize
+        // setting n to 8192
+        // 32 * 256 = 8192
+        @8192
+        D=A
+
+        @n
+        M=D
+
+        // init screen variable which is going to be incremented
+        @SCREEN
+        D=A
+
+        @n
+        M=M+D
+
+        @screen
+        M=D
+
+        (FILL_LOOP)
+            // set screen to color
+            @color
+            D=M
+
+            @screen
+            A=M
+            M=D
+
+            // increment screen
+            @screen
+            M=M+1
+
+            // compare i to n
+            @n
+            D=M
+
+            @screen
+            D=D-M
+
+            @FILL_LOOP
+            D;JGT
+
+            @READ_INPUT
+            0;JMP
--- nand2tetris/projects/04/mult/Mult.asm
+++ nand2tetris/projects/04/mult/Mult.asm
@@ -10,3 +10,77 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+    // Idea for loop 0..RAM[0] summing up RAM[1]
+    // first take the infer smaller value and init, n = R0 < R1 ? R0 : R1
+    @R0
+    D=M
+
+    @R1
+    D=D-M
+
+    @OVER_R1
+    D;JGE
+
+    @OVER_R0
+    D;JLT
+
+    (OVER_R1)
+        @R1
+        D=M
+
+        @n
+        M=D
+
+        @R0
+        D=M
+
+        @inc
+        M=D
+
+        @SUM
+        0;JMP
+
+    (OVER_R0)
+        @R0
+        D=M
+
+        @n
+        M=D
+
+        @R1
+        D=M
+
+        @inc
+        M=D
+
+
+        @SUM
+        0;JMP
+
+        (SUM)
+            @R2
+            M=0
+
+            (SUM_LOOP)
+                @n
+                D=M
+
+                @END
+                D;JLE
+
+                @inc
+                D=M
+
+                @R2
+                M=M+D
+
+                @n
+                M=M-1
+
+                @SUM_LOOP
+                0;JMP
+
+    (END)
+    @END
+    0;JMP
