49dee442e760628060dfbde7f4e4c4f27ebd9378
Kay Freyer <kay.freyer@icloud.com>
2024-03-26 07:12:15 +0100

    Add RoamFiles

---
+++ .dir-locals.el
@@ -0,0 +1,2 @@
+((nil . ((org-roam-directory . "/home/kay/workspace/from-nand-to-tetris/org-roam-files/")
+         (org-roam-db-location . "/home/kay/workspace/from-nand-to-tetris/org-roam-files/roam-nandtotetris.db"))))
